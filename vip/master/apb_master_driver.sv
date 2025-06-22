PKhuang-TW/pkhuang_apb_vip

commit 5492d3c1bde4beaaf3f082c4b93824ddbbdbd6a8

Refactor: move APB read/write protocol control from interface to driver

vip/master/apb_master_driver.sv
@@ -14,18 +14,35 @@ class apb_master_driver extends apb_driver_base;
             txn = apb_seq_item :: type_id :: create ("txn");
 
             seq_item_port.get_next_item(txn);
-            if ( txn.PWRITE ) begin
-                vif.apb_write (
-                    .sel ( txn.PSEL ),
-                    .addr ( txn.PADDR ),
-                    .data ( txn.PWDATA )
-                );
-            end else begin
-                vif.apb_read (
-                    .sel ( txn.PSEL ),
-                    .addr ( txn.PADDR )
-                );
+
+            if ( txn.PWRITE ) begin  // APB write
+                @ ( posedge PCLK );
+                PADDR   <= addr;
+                PSEL    <= sel;
+                PWDATA  <= data;
+                PWRITE  <= 1;
+
+                @ ( posedge PCLK );
+                PENABLE <= 1;
+
+                @ ( posedge PCLK );
+                @ ( posedge PREADY );
+                vif.reset_signal();
+                
+            end else begin  // APB read
+                @ ( posedge PCLK );
+                PADDR   <= addr;
+                PSEL    <= sel;
+                PWRITE  <= 0;
+
+                @ ( posedge PCLK );
+                PENABLE <= 1;
+
+                @ ( posedge PCLK );
+                @ ( posedge PREADY );
+                vif.reset_signal();
             end
+
             seq_item_port.item_done();
         end
     endtask

vip/interface/apb_interface.sv
@@ -27,42 +27,6 @@ interface apb_interface(
         input   PADDR, PWRITE, PSEL, PENABLE, PWDATA,
         output  PREADY, PRDATA
     );
-
-    task automatic apb_write (
-        input   logic                       sel,
-        input   logic [`D_ADDR_WIDTH-1:0]   addr,
-        input   logic [`D_DATA_WIDTH-1:0]   data
-    );
-        @ ( posedge PCLK );
-        PADDR   <= addr;
-        PSEL    <= sel;
-        PWDATA  <= data;
-        PWRITE  <= 1;
-
-        @ ( posedge PCLK );
-        PENABLE <= 1;
-
-        @ ( posedge PCLK );
-        @ ( posedge PREADY );
-        reset_signal();
-    endtask
-
-    task automatic apb_read (
-        input   logic                       sel,
-        input   logic [`D_ADDR_WIDTH-1:0]   addr
-    );
-        @ ( posedge PCLK );
-        PADDR   <= addr;
-        PSEL    <= sel;
-        PWRITE  <= 0;
-
-        @ ( posedge PCLK );
-        PENABLE <= 1;
-
-        @ ( posedge PCLK );
-        @ ( posedge PREADY );
-        reset_signal();
-    endtask
     
     task automatic reset_signal();
         PADDR   <= '0;

commit 2b5dc84d724bd443013e0063090e9ded6cd4cf9b

Fix: fix compile error
Fix the error caused by 5492d3c1bde4beaaf3f082c4b93824ddbbdbd6a8

vip/master/apb_master_driver.sv
@@ -16,30 +16,30 @@ class apb_master_driver extends apb_driver_base;
             seq_item_port.get_next_item(txn);
 
             if ( txn.PWRITE ) begin  // APB write
-                @ ( posedge PCLK );
-                PADDR   <= addr;
-                PSEL    <= sel;
-                PWDATA  <= data;
-                PWRITE  <= 1;
+                @ ( posedge vif.PCLK );
+                vif.PADDR   <= txn.PADDR;
+                vif.PSEL    <= txn.PSEL;
+                vif.PWDATA  <= txn.PWDATA;
+                vif.PWRITE  <= 1;
 
-                @ ( posedge PCLK );
-                PENABLE <= 1;
+                @ ( posedge vif.PCLK );
+                vif.PENABLE <= 1;
 
-                @ ( posedge PCLK );
-                @ ( posedge PREADY );
+                @ ( posedge vif.PCLK );
+                @ ( posedge vif.PREADY );
                 vif.reset_signal();
                 
             end else begin  // APB read
-                @ ( posedge PCLK );
-                PADDR   <= addr;
-                PSEL    <= sel;
-                PWRITE  <= 0;
+                @ ( posedge vif.PCLK );
+                vif.PADDR   <= txn.PADDR;
+                vif.PSEL    <= txn.PSEL;
+                vif.PWRITE  <= 0;
 
-                @ ( posedge PCLK );
-                PENABLE <= 1;
+                @ ( posedge vif.PCLK );
+                vif.PENABLE <= 1;
 
-                @ ( posedge PCLK );
-                @ ( posedge PREADY );
+                @ ( posedge vif.PCLK );
+                @ ( posedge vif.PREADY );
                 vif.reset_signal();
             end
 