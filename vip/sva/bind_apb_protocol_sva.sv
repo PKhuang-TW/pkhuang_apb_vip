8c94ae5922f8a137958b08e56bece417f85688ed
PKhuang-TW <pokaihuang.tw@gmail.com>
2025-06-28 00:56:57 +0800

    Fix: update top module name

--- vip/sva/bind_apb_protocol_sva.sv
+++ vip/sva/bind_apb_protocol_sva.sv
@@ -1,7 +1,7 @@
 `ifndef BIND_APB_PROTOCOL_SVA_SV
 `define BIND_APB_PROTOCOL_SVA_SV
 
-bind tb_top apb_protocol_sva bind_apb_sva (
+bind sim_top apb_protocol_sva bind_apb_sva (
     .PCLK(vif.PCLK),
     .PRESETn(vif.PRESETn),
     .PADDR(vif.PADDR),
